0cec9cdf9ec51f16e7d78dee1c7a47f6fc6df3bc
Robert Ladstätter <robert@ladstaetter.info>
2013-01-01 18:18:15 +0100

    adding jemmy dependencies and a simple scalatest
    adding jemmy dependencies and a simple scalatest

---
+++ src/main/resources/archetype-resources/src/test/scala/org/example/scalajfx/JfxApplicationSpec.scala
@@ -0,0 +1,26 @@
+package org.example.scalajfx
+
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.FlatSpec
+import org.jemmy.fx.SceneDock
+import org.jemmy.fx.AppExecutor
+import org.jemmy.fx.control.ToggleButtonDock
+import org.jemmy.fx.control.LabeledDock
+
+class JfxApplicationSpec extends FlatSpec with ShouldMatchers {
+
+  AppExecutor.executeNoBlock(classOf[JfxApplication])
+
+  val scene = new SceneDock
+
+  "a button" must "be present." in {
+    val b = new LabeledDock(scene.asParent)
+    println(b.asText().text())
+  }
+
+  "hellow world" should "be shown on the console." in {
+    val mouseAtButton = new LabeledDock(scene.asParent()).mouse()
+    for (i <- 1 to 10) mouseAtButton.click
+  }
+  
+}
