21b39d999149bfae25e3f01fee6fe60317b19621
Robert Ladstätter <robert@ladstaetter.info>
2013-01-02 20:43:50 +0100

    added a test fixture

--- src/main/resources/archetype-resources/src/test/scala/org/example/scalajfx/JfxApplicationSpec.scala
+++ src/main/resources/archetype-resources/src/test/scala/org/example/scalajfx/JfxApplicationSpec.scala
@@ -15,12 +15,14 @@ class JfxApplicationSpec extends FlatSpec with ShouldMatchers {
 
   "a button" must "be present." in {
     val b = new LabeledDock(scene.asParent)
-    println(b.asText().text())
+    b.asText().text() should be ("Click Me")
   }
 
   "hello world" should "be shown ten times on the console." in {
     val mouseAtButton = new LabeledDock(scene.asParent()).mouse()
     for (i <- 1 to 10) mouseAtButton.click
   }
+  
+  // .... 
   
 }
